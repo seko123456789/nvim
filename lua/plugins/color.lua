f211e7b749a046a3badb97d98aadf516b9bf1f3a
root <root@DESKTOP-B3E714T>
2024-10-26 15:49:53 +0300

    gruv

--- lua/plugins/color.lua
+++ lua/plugins/color.lua
@@ -1,18 +1,8 @@
 return {
-    {
-        "folke/tokyonight.nvim",
-        config = function()
-            require("tokyonight").setup({
-                style = "storm",
-                transparent = false, -- Enable this to disable setting the background color
-                terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
-                styles = {
-                    comments = { italic = false },
-                    keywords = { italic = false },
-                    sidebars = "dark", -- style for sidebars, see below
-                    floats = "dark", -- style for floating windows
-                },
-            })
-        end
-    },
+    "ellisonleao/gruvbox.nvim",
+    as = "gruvbox",
+    priority = 1000,
+    config = function()
+        vim.cmd("colorscheme gruvbox")
+    end,
 }
