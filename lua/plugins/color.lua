5a41b55730054c461b55937d22e9db2c26b428de
root <root@DESKTOP-B3E714T>
2024-10-26 15:45:56 +0300

    added tokyonight

--- lua/plugins/color.lua
+++ lua/plugins/color.lua
@@ -1,11 +1,22 @@
 return {
-  "thimc/gruber-darker.nvim",
-  config = function()
-    require("gruber-darker").setup({
-      transparent = true, -- removes the background
-      underline = true, -- disables underline fonts
-      bold = true,     -- disables bold fonts
-    })
-    vim.cmd.colorscheme("gruber-darker")
-  end,
+    {
+        "folke/tokyonight.nvim",
+        config = function()
+            require("tokyonight").setup({
+               -- or leave it empty to use the default settings
+                style = "storm",
+                transparent = true, -- Enable this to disable setting the background color
+                terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+                styles = {
+                    -- Style to be applied to different syntax groups
+                    -- Value is any valid attr-list value for `:help nvim_set_hl`
+                    comments = { italic = false },
+                    keywords = { italic = false },
+                    -- Background styles. Can be "dark", "transparent" or "normal"
+                    sidebars = "dark", -- style for sidebars, see below
+                    floats = "dark", -- style for floating windows
+                },
+            })
+        end
+    },
 }
